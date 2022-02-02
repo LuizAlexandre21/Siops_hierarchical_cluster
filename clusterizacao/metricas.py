78066ed4c18bbb02ebab700579349816e79a03eb
LuizAlexandre21 <luizalexandre21@outlook.com>
2022-02-02 00:29:27 -0300

    Update metrics function

--- clusterizacao/metricas.py
+++ clusterizacao/metricas.py
@@ -2,6 +2,8 @@
 from sklearn.metrics import adjusted_mutual_info_score, adjusted_rand_score, completeness_score, fowlkes_mallows_score, homogeneity_score, mutual_info_score, normalized_mutual_info_score, rand_score, v_measure_score, f1_score, precision_score, recall_score
 from cluster import *
 from data_treatment import data
+import pandas as pd 
+
 # Dados 
 X_train, X_test, y_train, y_test = data().main()
 
@@ -24,6 +26,8 @@ for method in [Agglomerative,Spectral,Optics,Meanshift,BIRCH,Dbscan,Affinity,Knn
     Metricas['Normalized_mutual_info_score'].append(normalized_mutual_info_score(y_test.values.T[0], y_pred))
     Metricas['V_measure_score'].append(v_measure_score(y_test.values.T[0], y_pred))
 
+def metrics():
+    return Metricas
 
 # Exportando 
 pd.DataFrame(Metricas).to_csv("Metricas.csv")
